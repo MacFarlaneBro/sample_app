8e9c15e39efc111e45ed532a84401b5c5ec71cd1
Charlie Brodie <charliembrodie@gmail.com>
2014-06-18 14:43:35 +0100

    first commit

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Charlie Brodie"
+    email    "charliembrodie@gmail.com"
+    password "blouse8"
+    password_confirmation "blouse8"
+  end
+end
