f7d35570eb0d29ac3a62e0d5b2a0e56ff0e78501
Charlie Brodie <charliembrodie@gmail.com>
2014-07-22 16:42:31 +0100

    Revert "what"
    This reverts commit b9272bab71c085e26e5e0d4820816e6a1d4b4de2.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    redirect_to(root_url)
+    @user = User.new(user_params)  # Not the final implementation!
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
