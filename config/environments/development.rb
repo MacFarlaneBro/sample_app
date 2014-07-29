d88b3cf0c81e18ed50b0c15a76eb7a255aada9d6
Charlie Brodie <charliembrodie@gmail.com>
2014-07-29 16:25:17 +0100

    I've now finished the majority of the config for the Forum, next stage is to really get to grips with RSpec and write a comprehensive battery of tests.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ SampleApp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Defines the default URL to be used with Forem.
+  config.action_mailer.default_url_options = { :host => 'localhost' }
 end
