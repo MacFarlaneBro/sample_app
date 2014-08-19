cd949380f26d04fc8a39f011c5c34a019b48c9cf
Charlie Macfarlane Brodie <charliembrodie@gmail.com>
2014-08-19 10:11:16 +0100

    I left a typo in the environment file, committing the correction.

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 SampleApp::Application.initialize!
 
-config.action_mailer.deliver_method = :smtp
-
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
