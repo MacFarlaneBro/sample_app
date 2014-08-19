dcaf75a2daddf17cfcd3012a0717069f777281f6
Charlie Macfarlane Brodie <charliemacfarlanebrodie@charlie.local>
2014-08-19 09:51:38 +0100

    configured a sendgrid mail client to work with the heroku app, to fix the issues I was having with replying to comments

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,14 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 SampleApp::Application.initialize!
 
+config.action_mailer.deliver_method = :smtp
 
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
--- db/migrate/20140818102735_remove_spam_tag_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSpamTagFromUser < ActiveRecord::Migration
-  def change
-    User.find_by_email("charliembrodie@gmail.com").update_attribute :forem_state, "approved"
-  end
-end
